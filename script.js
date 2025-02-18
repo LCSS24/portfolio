bfbae4c78d6340c61cfad5a27c87431891076e4d
Lucas oui <lucas.carpentier24@gmail.com>
2025-02-18 16:54:54 +0100

    Structure quasi finie

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-document.addEventListener("scroll", function () {
-  const header = document.querySelector(".barreheader");
-  const section = document.getElementById("Apropos");
-  const rect = section.getBoundingClientRect();
+function headerScroll() {
+  document.addEventListener("scroll", function () {
+    const header = document.querySelector(".barreheader");
+    const section = document.getElementById("Apropos");
+    const rect = section.getBoundingClientRect();
 
-  if (rect.top <= 0) {
-    header.classList.add("barreheader2");
-  } else {
-    header.classList.remove("barreheader2");
-  }
-});
-
-const images = document.querySelectorAll(".imgcontainer img");
-let currentIndex = 0;
-
-function showImage(index) {
-  images[currentIndex].classList.remove("active");
-  currentIndex = (index + images.length) % images.length;
-  images[currentIndex].classList.add("active");
+    if (rect.top <= 0) {
+      header.classList.add("barreheader2");
+    } else {
+      header.classList.remove("barreheader2");
+    }
+  });
 }
 
-document.querySelector(".gauche").addEventListener("click", () => {
-  showImage(currentIndex - 1);
-});
+function carouselDesc() {
+  const images = document.querySelectorAll(".imgcontainer img");
+  let currentIndex = 0;
+  const descriptions = document.querySelectorAll(".description");
 
-document.querySelector(".droite").addEventListener("click", () => {
-  showImage(currentIndex + 1);
-});
+  function showImage(index) {
+    images[currentIndex].classList.remove("active");
+    descriptions[currentIndex].classList.remove("descactive");
+    currentIndex = (index + images.length) % images.length;
+    images[currentIndex].classList.add("active");
+    descriptions[currentIndex].classList.add("descactive");
+  }
+
+  document.querySelector(".gauche").addEventListener("click", () => {
+    showImage(currentIndex - 1);
+  });
+
+  document.querySelector(".droite").addEventListener("click", () => {
+    showImage(currentIndex + 1);
+  });
+}
+
+function main() {
+  carouselDesc();
+  headerScroll();
+}
+
+document.addEventListener("DOMContentLoaded", main);
